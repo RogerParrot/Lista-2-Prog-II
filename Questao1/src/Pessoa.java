9fad9311bc952e810b389c1ad899580db6ed6f3a
RogerParrot <rogeriodosanjosbarbosa2004@gmail.com>
2024-12-17 23:22:05 -0300

    first commit

---
+++ Questao1/src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    private String nome, email, telefone;
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
