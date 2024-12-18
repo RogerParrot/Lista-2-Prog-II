923f6406b3e74861965d9f0bc1251db001c427b4
RogerParrot <rogeriodosanjosbarbosa2004@gmail.com>
2024-12-17 23:46:45 -0300

    Remodelando a main da questão 1 e criando o projeto da questão 2

--- Questao1/src/Main.java
+++ Questao1/src/Main.java
@@ -1,14 +1,29 @@
 public class Main {
-    public Universidade ufpa = new Universidade("ufpa");
+    public static void main(String args[]){
+        Universidade ufpa = new Universidade("ufpa");
 
-    public Departamento secretaria = new Departamento("secretaria");
-    public Departamento diretoria = new Departamento("diretoria");
-    public Departamento corpoDocente = new Departamento("corpo docente");
+        Departamento secretaria = new Departamento("secretaria");
+        Departamento diretoria = new Departamento("diretoria");
+        Departamento corpoDocente = new Departamento("corpo docente");
 
-    public Pessoa diretor = new Pessoa("diretor", "diretor@ufpa.br", "91987654321");
-    public Pessoa viceDiretor = new Pessoa("vice diretor", "vicediretor@ufpa.br", "91912345678");
-    public Pessoa secretario = new Pessoa("secretario", "secretario@ufpa.br", "91999998888");
-    public Pessoa monitor = new Pessoa("monitor", "monitor@ufpa.br", "91988887777");
-    public Pessoa professor1 = new Pessoa("professor 1", "professor1@ufpa.br", "91999999999");
-    public Pessoa professor2 = new Pessoa("professor 2", "professor2@ufpa.br", "91988888888");
+        Pessoa diretor = new Pessoa("diretor", "diretor@ufpa.br", "91987654321");
+        Pessoa viceDiretor = new Pessoa("vice diretor", "vicediretor@ufpa.br", "91912345678");
+        Pessoa secretario = new Pessoa("secretario", "secretario@ufpa.br", "91999998888");
+        Pessoa monitor = new Pessoa("monitor", "monitor@ufpa.br", "91988887777");
+        Pessoa professor1 = new Pessoa("professor 1", "professor1@ufpa.br", "91999999999");
+        Pessoa professor2 = new Pessoa("professor 2", "professor2@ufpa.br", "91988888888");
+
+        ufpa.addDepartamento(secretaria);
+        ufpa.addDepartamento(diretoria);
+        ufpa.addDepartamento(corpoDocente);
+
+        diretoria.addPessoa(diretor);
+        diretoria.addPessoa(viceDiretor);
+
+        secretaria.addPessoa(secretario);
+        secretaria.addPessoa(monitor);
+
+        corpoDocente.addPessoa(professor1);
+        corpoDocente.addPessoa(professor2);
+    }
 }
